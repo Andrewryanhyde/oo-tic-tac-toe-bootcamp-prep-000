fb4ff8762992d3c4f3643cfae9a0ee9928f91d29
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-31 09:05:57 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,21 +1,21 @@
-class TicTacToe 
-  
-  def initialize 
-    @board = Array.new(9, " ")
-  end
-  
+class TicTacToe
+
   WIN_COMBINATIONS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [6, 4, 2]
-    ]
-  
-  def display_board
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+  ]
+
+def initialize
+  @board = Array.new(9, " ")
+end
+
+def display_board
   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
   puts "-----------"
   puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
@@ -23,24 +23,44 @@ class TicTacToe
   puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
 end
 
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
 
-  def input_to_index(user_input)
-    return user_input.to_i - 1
-  end
-  
-  def move(index, current_player)
+def move(index, current_player = "X")
   @board[index] = current_player
+end
+
+def position_taken?(index)
+  !(@board[index].nil? || @board[index] == " ")
+end
+
+def valid_move?(index)
+  index.between?(0,8) && !position_taken?(index)
+end
+
+def turn_count
+  turn = 0
+  @board.each do |index|
+    if index == "X" || index == "O"
+      turn += 1
+    end
   end
-  
-  def position_taken?(index)
-  !(@board[index].nil? || @board[index] == "  ")
+  return turn
+end
+
+def current_player
+  #if the turn count is an even number, that means O just went, so the next/current player is X
+  num_turns = turn_count
+  if num_turns % 2 == 0
+    player = "X"
+  else
+    player = "O"
   end
-  
-  def valid_move?(index)
-  index.between?(0,8) && !position_taken?(@board, index)
-  end
-  
-  def turn
+  return player
+end
+
+def turn
   puts "Please choose a number 1-9:"
   user_input = gets.chomp
   index = input_to_index(user_input)
@@ -51,27 +71,7 @@ end
   else
     turn
   end
-  end
-  
-  def turn_count
-  turn = 0
-  @board.each do |index|
-    if index == "X" || index == "O"
-      turn += 1
-    end
-  end
-  return turn
-  end
-  
-  def current_player
-  num_turns = turn_count
-  if num_turns % 2 == 0
-    player = "X"
-  else
-    player = "O"
-  end
-  return player
-  end
+end
 
 def won?
   WIN_COMBINATIONS.each {|win_combo|
@@ -137,6 +137,5 @@ def play
     puts "Cat's Game!"
   end
 end
-  
-  
+
 end
